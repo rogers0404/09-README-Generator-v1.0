5cd7b3626dca976d9cc7eb7ed7e2db29bc4a76ab
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-10-03 12:07:52 -0400

    Added template for README.md

--- src/readme-template.js
+++ src/readme-template.js
@@ -1,4 +1,4 @@
-////////////////// Confirm Usage /////////////////////////////////
+////////////////// Confirm Function /////////////////////////////////
 
 const getMessage = (confirm, message) => { 
     if(confirm) 
@@ -24,6 +24,106 @@ const getBadge = license =>{
     }
 };
 //////////////////////////////////////////////////////////////////
+
+////////////////// Getting Badge /////////////////////////////////
+
+const getBadge = license =>{
+    switch(license) {
+        case "Apache License 2.0":
+          return "https://img.shields.io/static/v1?label=License&message=Apache&color=red&&style=plastic";
+        case "GNU General Public License v3.0":
+          return "https://img.shields.io/static/v1?label=License&message=GNU&color=yellowgreen&&style=plastic";
+        case "Mozilla Public License 2.0":
+            return "https://img.shields.io/static/v1?label=License&message=Mozilla&color=orange&&style=plastic";
+        case "MIT License ":
+            return "https://img.shields.io/static/v1?label=License&message=MIT&color=green&&style=plastic";
+        case "None ":
+            return "https://img.shields.io/static/v1?label=License&message=None&color=inactive&&style=plastic";    
+    }
+};
+
+////////////////// Getting Text for License /////////////////////////////////
+
+const getDiscloseLicene = (license, title) =>{
+    switch(license) {
+        case "Apache License 2.0":
+            return `
+                ${title} is licensed under the
+
+                ${getLicense(licence)}
+                
+                ${licence}
+                A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+                
+                Permissions | Limitations  |   Conditions
+                ------------ | -------------
+                Commercial use | Liability |  License and copyright notice
+                Modification | Warranty    |  State changes
+                Distribution | Trademark use   |
+                Private use |     |
+                Patent use |     |
+            `;
+        case "GNU General Public License v3.0":
+            return `
+                ${title} is licensed under the
+
+                ${getLicense(licence)}
+                
+                ${licence}
+                Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.
+
+                Permissions | Limitations  |   Conditions
+                ------------ | -------------
+                Commercial use | Liability |  License and copyright notice
+                Modification | Warranty    |  State changes
+                Distribution |   |  Disclose source
+                Private use |     |     Same license
+                Patent use |     |
+            `;
+        case "Mozilla Public License 2.0":
+            return `
+                ${title} is licensed under the
+
+                ${getLicense(licence)}
+                
+                ${licence}
+                Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work
+ 
+                Permissions | Limitations  |   Conditions
+                ------------ | -------------
+                Commercial use | Liability |  License and copyright notice
+                Modification | Warranty    |  Same license (file)
+                Distribution | Trademark use  |  Disclose source
+                Private use |     |     
+                Patent use |     |
+            `;
+        case "MIT License ":
+            return `
+                ${title} is licensed under the
+
+                ${getLicense(licence)}
+                
+                ${licence}
+                A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+
+                Permissions | Limitations  |   Conditions
+                ------------ | -------------
+                Commercial use | Liability |   License and copyright notice
+                Modification | Warranty    |
+                Distribution |     |
+                Private use |     |
+            `;
+        case "None ":
+            return `
+                ${title} is licensed under the
+
+                ${getLicense(licence)}
+                
+                ${licence}
+            
+            `;    
+    }
+};
 
 //////////////// README Template /////////////////////////////////
 module.exports = templateData => {
@@ -63,9 +163,7 @@ ${getMessage(confirmContribution, contribution)}
 
 ## License
 
-
-[https://choosealicense.com/](https://choosealicense.com/)
-
+${getDiscloseLicene(license, title)}
 
 ## Tests
 
